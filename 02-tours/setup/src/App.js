d8857a89e6151144081a751a4541b6faaae1f210
Mayank Agrawal <mayankagrawal0004@gmail.com>
2023-10-16 23:45:58 -0400

    changes

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -7,12 +7,41 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading,setLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id)
+    setTours(newTour);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try{
+    const respone = await fetch(url);
+    const data = await respone.json();
+    setLoading(false);
+    setTours(data);
+    } catch(error){
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  },[]) 
   if(loading){
     return <main>
       <Loading/>
     </main>
   }
-  return <h2>Tours Project Setup</h2>
+  if(tours.length === 0){
+    return (<main>
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>Refresh</button>
+    </div>
+    </main>)
+  }
+  return <main><Tours tours={tours} removeTour={removeTour}/></main>
 }
 
 export default App
