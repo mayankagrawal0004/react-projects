dab78cb6c6b5b7831e6245556a5a0e244e902f3b
Mayank Agrawal <mayankagrawal0004@gmail.com>
2023-10-16 23:07:54 -0400

    changes

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,6 +5,13 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
+  const [loading,setLoading] = useState(true);
+  const [tours,setTours] = useState([]);
+  if(loading){
+    return <main>
+      <Loading/>
+    </main>
+  }
   return <h2>Tours Project Setup</h2>
 }
 
